ea1a46ca9c837fced918f1505ee8246229ac7f8b
thejakgithub <jak71646167@gmail.com>
2021-12-20 14:49:53 +0700

    add qrcode line

--- frontend/src/components/Title.vue
+++ frontend/src/components/Title.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="wrapper__title">
+  <div class="wrapper__title lg:block flex flex-col">
     <lottie-player
       class="lottie"
       src="https://assets4.lottiefiles.com/packages/lf20_Q2blbQ.json"
@@ -7,14 +7,25 @@
       speed="1"
       autoplay
     ></lottie-player>
-    <h2 class="fadeIn">LAUNDROMAT<br />COMPANY</h2>
-    <p class="mymove">
-      <span>For machine - washable wool</span>
-      <br />
-      Loads should be under 8 pounds. It is recommended using a neutral
-      detergent to prevent damage to the cloth and to improve the washing
-      results
-    </p>
+    <div>
+      <h2 class="fadeIn">LAUNDROMAT COMPANY</h2>
+    </div>
+    <div class="lg:block flex lg:mb-0">
+      <img
+        src="../assets/img/qrcodeline.jpg"
+        alt="qrcodeline"
+        width="200"
+        height="200"
+        class="lg:absolute static top-0 right-0 mt-4 mr-4"
+      />
+      <p class="mymove">
+        <span>For machine - washable wool</span>
+        <br />
+        Loads should be under 8 pounds. It is recommended using a neutral
+        detergent to prevent damage to the cloth and to improve the washing
+        results
+      </p>
+    </div>
   </div>
 </template>
 
